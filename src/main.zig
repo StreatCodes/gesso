55a9aa6f73601eb2892a445cbacbc8c2bb2c3504
Matthew Streatfield <matt.r.streatfield@gmail.com>
2026-05-29 22:54:11 +1000

    Upgrade to 0.16

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,11 @@
 const std = @import("std");
 const gesso = @import("gesso");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+pub fn main(init: std.process.Init) !void {
+    const gpa = init.gpa;
 
     var instance = try gesso.init("Gesso", 1280, 720);
     defer instance.deinit();
 
-    while (try instance.handleInput(allocator)) {}
+    while (try instance.handleInput(gpa)) {}
 }
